13928407f89890e7c78b7e8e710ae5e930cdb4da
Tropico Finance <tropicofinance@protonmail.com>
2021-03-04 18:01:59 +0100

    update

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,5 +1,5 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
+  cakeAddress: '0xa6477c795dA4B68813EA167660145464D89e6646',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
   lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
 }
